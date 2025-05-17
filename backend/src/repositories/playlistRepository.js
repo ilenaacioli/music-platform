6875bd2fb8b0355d43e049d52ec2c5c400203187
ilenaacioli <ilenaaciolids@gmail.com>
2025-05-17 17:58:29 -0300

    feat: adicionando repositorios

---
+++ backend/src/repositories/playlistRepository.js
@@ -0,0 +1,31 @@
+import db from "../db.js"
+
+export class PlaylistRepository {
+    constructor(
+    ) {}
+
+    getList = async (name) => {
+        const query = db('playlists')
+
+        if (name) {
+          query.where('name', 'like', `%${name}%`)
+        }
+    
+        return await query
+    }
+
+    create = async (name, description, editable) => {
+        const playlists = await db('playlists').insert({  name, description, editable })
+        return playlists
+        }
+
+    edit = async (updateData,id ) => {
+            const playlist = await db('playlists')
+            .where({ id})
+            .update(updateData)
+            return playlist
+    }
+
+            
+
+  }
---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,17 @@
+import db from "../db.js"
+
+export class UserRepository {
+    constructor(
+    ) {}
+
+    getByEmail = async (email) => {
+      const user = await db('users').where({email}).first()
+      return user
+    }
+
+    create = async (email,name, password) => {
+    const user = await db('users').insert({ email, name, password })
+    return user
+    }
+
+  }
