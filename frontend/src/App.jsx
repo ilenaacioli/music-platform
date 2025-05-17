c3118aee799c441fff42a02cfa4dc64681234ae2
ilenaacioli <ilenaaciolids@gmail.com>
2025-05-17 09:55:48 -0300

    feat: estrutura front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  const [playlists, setPlaylists] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/playlists')
-      .then(res => setPlaylists(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
-  return (
-    <div>
-      <h1>Playlists</h1>
-      <ul>
-        {playlists.map(p => (
-          <li key={p.id}>{p.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
