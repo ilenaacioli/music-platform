2f5a58f89729bd313ac4b0112e33db2f8364a1d3
ilenaacioli <ilenaaciolids@gmail.com>
2025-05-17 11:11:44 -0300

    feat: migration

---
+++ backend/src/migrations/20240516120000_initial_schema.js
@@ -0,0 +1,52 @@
+exports.up = function (knex) {
+    return knex.schema
+      .createTable('users', (table) => {
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('email').unique().notNullable()
+        table.string('password').notNullable()
+        table.timestamps(true, true) 
+      })
+      .createTable('playlists', (table) => {
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.text('description')
+        table.boolean('editable').notNullable().defaultTo(false)
+        table.timestamps(true, true)
+      })
+      .createTable('music', (table) => {
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('artist').notNullable()
+        table.string('url').notNullable()
+        table
+          .integer('playlistId')
+          .unsigned()
+          .references('id')
+          .inTable('playlists')
+        table.integer('duration') 
+        table.timestamps(true, true)
+      })
+      .createTable('favorites', (table) => {
+        table.increments('id').primary()
+        table
+          .integer('userId')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+        table
+          .integer('musicId')
+          .unsigned()
+          .references('id')
+          .inTable('music')
+        table.timestamps(true, true)
+      })
+  }
+  
+  exports.down = function (knex) {
+    return knex.schema
+      .dropTableIfExists('favorites')
+      .dropTableIfExists('music')
+      .dropTableIfExists('playlists')
+      .dropTableIfExists('users')
+  }
