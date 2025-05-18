57dd4d5c00431eb8527075d3be643ca64a3b3c1c
ilenaacioli <ilenaaciolids@gmail.com>
2025-05-18 14:55:30 -0300

    fix: ajuste arquivos banco dados

--- backend/knexfile.cjs
+++ backend/knexfile.cjs
@@ -1,17 +1,17 @@
-export default {
+module.exports = {
   development: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'musicdb'
+      database: process.env.DB_NAME || 'musicdb',
     },
     migrations: {
-      directory: './src/migrations'
+      directory: './src/migrations',
     },
     seeds: {
-      directory: './src/seeds'
-    }
-  }
+      directory: './src/seeds',
+    },
+  },
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ import express, { json } from 'express'
 const app = express()
 
 import userRoutes from './routes/user.js'
-import playlistRoutes from './routes/playlists.js' 
+import playlistRoutes from './routes/playlists.js'
+import musicRoutes from './routes/music.js'
+
 app.use(json())
 app.use(userRoutes)
 app.use(playlistRoutes)
+app.use(musicRoutes)
 
 const PORT = 5000
 app.listen(PORT, () => {
