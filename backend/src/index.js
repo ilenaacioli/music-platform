c08e6d179324b3ccdb973c6f5098c557c6615c2f
ilenaacioli <ilenaaciolids@gmail.com>
2025-05-21 22:47:26 -0300

    fix: configuracao porta

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
 import express, { json } from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -13,7 +16,8 @@ app.use(userRoutes)
 app.use(playlistRoutes)
 app.use(musicRoutes)
 
-const PORT = 5000
+const PORT = process.env.BACKEND_PORT || 5000
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_FRONTEND_PORT) || 3000,
+    },
+  })
+}
